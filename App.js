0c3e38364cae35b8009d378c587223ab9968fee6
Christoph Lambio <c.lambio@datalyze-solutions.com>
2018-03-27 17:06:39 +0200

    Weiter vereinfacht

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
   View,
   Dimensions
 } from 'react-native';
-import MapView from 'react-native-maps';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import MapView, { MbTile } from 'react-native-maps';
 
 const { width, height } = Dimensions.get('window');
 
@@ -30,8 +15,7 @@ const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 const SPACE = 0.01;
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View
@@ -47,6 +31,7 @@ export default class App extends Component<Props> {
           loadingEnabled
           loadingIndicatorColor="#666666"
           loadingBackgroundColor="#eeeeee">
+          <MbTile />
         </MapView>
       </View>
     )

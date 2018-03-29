250b0b4d3a39d928921bd8be9e78e806db4164f0
Christoph Lambio <c.lambio@datalyze-solutions.com>
2018-03-29 17:00:35 +0200

    Dummy data added

--- App.js
+++ App.js
@@ -2,25 +2,47 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Dimensions
+  Dimensions,
+  TouchableOpacity,
+  Button,
+  Text,
 } from 'react-native';
 import MapView from 'react-native-maps';
 
 const { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-const LATITUDE = 48.67483002913051;
-const LONGITUDE = -113.76805078249163
-const LATITUDE_DELTA = 0.922;
+const LATITUDE = 23.736906;
+const LONGITUDE = 90.397768;
+const LATITUDE_DELTA = 0.022;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 const SPACE = 0.01;
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      offlineMap: false
+    };
+  }
+
+  _toggleOfflineMap = () => {
+    this.setState({
+      offlineMap: !this.state.offlineMap
+    });
+  }
+
   render() {
     return (
       <View
         style={styles.container}>
+        <TouchableOpacity
+          style={{ position: "absolute", bottom: 0, backgroundColor: this.state.offlineMap ? "lightblue" : "white", zIndex: 100, height: 50, width: width, justifyContent: "center", alignItems: "center" }}
+          onPress={() => this._toggleOfflineMap()}>
+          <Text> {this.state.offlineMap ? "Online Map" : "Offline Map"} </Text>
+        </TouchableOpacity>
         <MapView
           style={styles.map}
           initialRegion={{
@@ -32,14 +54,12 @@ export default class App extends Component<Props> {
           loadingEnabled
           loadingIndicatorColor="#666666"
           loadingBackgroundColor="#eeeeee">
-          <MapView.MbTile
-            pathTemplate={"MbTilePath"}
-            tileSize={256} />
-          <MapView.LocalTile
-            pathTemplate={"LocalTilePath"}
-            tileSize={256} />
+          {this.state.offlineMap ? <MapView.LocalTile
+            pathTemplate={"{z}/{x}/{y}"}
+            tileSize={256} /> : null}
+
         </MapView>
-      </View>
+      </View >
     )
   }
 }
@@ -47,8 +67,6 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
